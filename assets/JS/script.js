a6e8152e5fbd0300c5493fcba6c7a137a64b0f4e
Jacob Rousseau <111393639+jakesouleater@users.noreply.github.com>
2026-07-30 21:04:04 -0400

    Loading screen
    Adding loading screen

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -13,7 +13,9 @@ const API_KEY = "PNsq3lZQkAJMPHIPXjsWL2fXT9iGgW15";
 // ======================================================
 // PLAYER SEARCH
 // ======================================================
+const noPlayer = document.getElementById("no-player");
 const orgInfo = document.getElementById("org-info");
+const noCitizen = document.getElementById("no-citizen")
 const noOrg = document.getElementById("no-org");
 const searchButton = document.getElementById("search-button");
 const searchResults = document.getElementById("search-results");
@@ -25,49 +27,82 @@ const displayMembers = document.getElementById("display-members");
 const profilePicture = document.getElementById("display-profile-picture");
 const orgImage = document.getElementById("org-img");
 const searchForm = document.getElementById("search-form");
-// const loadingScreen = document.getElementById("loading-screen");
+const loadingScreen = document.getElementById("loading-screen");
+const loadingMessage = document.getElementById("loading-message");
 
-searchForm.addEventListener("submit", async function (event) {
+
+
+  searchForm.addEventListener("submit", async function (event) {
   event.preventDefault();
-  orgImage.src = "";
-  displayOrg.textContent = "";
-  displayRank.textContent = "";
-  displayMembers.textContent = "";
 
-  // Read the player name entered by the user.
-  const playerName = document.getElementById("player-name").value;
+  loadingScreen.style.display = "block";
 
-  // Request player information from the API.
-  const url = `https://api.starcitizen-api.com/${API_KEY}/v1/live/user/${playerName}`;
-  const response = await fetch(url);
-  const playerData = await response.json();
+    async function typeMessage(element, message, speed){
 
-  // --------------------------------------------------
-  // Display Player Information
-  // --------------------------------------------------
+      element.textContent = "";
 
-  profilePicture.src = playerData.data.profile.image;
-  displayHandle.textContent = playerData.data.profile.handle;
-  displayEnlisted.textContent = playerData.data.profile.enlisted.slice(0, 10);
-  console.log(playerData.data);
-  searchResults.style.display = "flex";
-  // --------------------------------------------------
-  // Organization Information
-  // --------------------------------------------------
+      for (let i = 0; i < message.length; i++){
+        element.textContent = message.slice(0, i);
+        await wait(speed);
 
-  const orgSID = playerData.data.organization.sid;
+      }
 
-  if (playerData.data.organization.name === undefined) {
-    orgInfo.style.display = "none";
-    noOrg.style.display = "block";
-    noOrg.textContent = "No Organization";
-    // TODO:
-    // Hide organization information.
-    // Show "No Organization".
-  } else {
-    noOrg.style.display = "none";
+    };
+
+    await typeMessage(loadingMessage, "Initializing COMM-LINK...", 50);
+
+  try {
+
+    orgImage.src = "";
+    displayOrg.textContent = "";
+    displayRank.textContent = "";
+    displayMembers.textContent = "";
+
+    // Read the player name entered by the user.
+    const playerName = document.getElementById("player-name").value;
+    const url = `https://api.starcitizen-api.com/${API_KEY}/v1/live/user/${playerName}`;
+
+    const response = await fetch(url);
+    const playerData = await response.json();
+
+    // --------------------------------------------------
+    // Display Player Information
+    // --------------------------------------------------
+
+    if (Object.keys(playerData.data).length === 0) {
+      orgInfo.style.display = "none";
+      noCitizen.style.display = "none";
+      noPlayer.style.display = "block";
+      noPlayer.textContent = "No Citizen Found";
+      searchResults.style.display = "flex";
+      return;
+    }
+
+    noPlayer.style.display = "none";
     orgInfo.style.display = "block";
 
+    displayHandle.textContent = playerData.data.profile.handle;
+    profilePicture.src = playerData.data.profile.image;
+    displayEnlisted.textContent = playerData.data.profile.enlisted.slice(0, 10);
+    searchResults.style.display = "flex";
+
+    // --------------------------------------------------
+    // Organization Information
+    // --------------------------------------------------
+
+    if (playerData.data.organization.sid === undefined) {
+      orgInfo.style.display = "none";
+      noOrg.style.display = "block";
+      noOrg.textContent = "No Organization";
+      return;
+    }
+
+    noPlayer.style.display = "none";
+    noOrg.style.display = "none";
+    orgInfo.style.display = "block";
+    noCitizen.style.display = "block";
+
+    const orgSID = playerData.data.organization.sid;
     const orgURL = `https://api.starcitizen-api.com/${API_KEY}/v1/live/organization/${orgSID}`;
 
     const organizationResponse = await fetch(orgURL);
@@ -77,5 +112,10 @@ searchForm.addEventListener("submit", async function (event) {
     displayOrg.textContent = playerData.data.organization.name;
     displayRank.textContent = playerData.data.organization.rank;
     displayMembers.textContent = organizationData.data.members;
+
+  } finally {
+
+    loadingScreen.style.display = "none";
+
   }
 });
